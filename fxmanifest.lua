6c8ee2b6b16da3d199fb52c6a184bca5a0d8ce4b
adammm5 <buddyandbucko@gmail.com>
2024-10-04 19:47:17 +1000

    Added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Adam'
+description 'Simple Pier Protection Script'
+version '1.0.0'
+
+shared_script '@ox_lib/init.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+lua54 'yes'
